06f138fe4168979cd59cb69d90a26b7ad7c6f4f0
DamsyDeSilva <damsydesilva@gmail.com>
2021-05-15 02:06:54 +0530

    hospital model added

---
+++ Back-End/ncms/src/main/java/com/sparkx/damsy/models/Hospital.java
@@ -0,0 +1,69 @@
+package com.sparkx.damsy.models;
+
+import java.util.Date;
+
+public class Hospital {
+
+    private String id;
+    private String name;
+    private String district;
+    private int locationX;
+    private int locationY;
+    private Date buildDate;
+    
+    public Hospital() {
+    }
+
+    public Hospital(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getLocationX() {
+        return locationX;
+    }
+
+    public void setLocationX(int locationX) {
+        this.locationX = locationX;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+
+    public void setLocationY(int locationY) {
+        this.locationY = locationY;
+    }
+
+    public Date getBuildDate() {
+        return buildDate;
+    }
+
+    public void setBuildDate(Date buildDate) {
+        this.buildDate = buildDate;
+    } 
+
+}
