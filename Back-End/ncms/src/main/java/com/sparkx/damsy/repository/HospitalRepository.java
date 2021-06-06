3a6059588e655b8def7a7b9b073e506b380be187
DamsyDeSilva <damsydesilva@gmail.com>
2021-06-07 03:11:33 +0530

    hospital repository & service updates

--- Back-End/ncms/src/main/java/com/sparkx/damsy/repository/HospitalRepository.java
+++ Back-End/ncms/src/main/java/com/sparkx/damsy/repository/HospitalRepository.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.sparkx.damsy.connections.DBConnectionPool;
 import com.sparkx.damsy.models.Hospital;
@@ -13,10 +14,13 @@ public class HospitalRepository {
     private static String GET_HOSPITAL_BY_ID_QUERY = "SELECT * FROM hospital WHERE id = ? LIMIT 1";
     private static String INSERT_HOSPITAL_QUERY = "Insert into hospital(id, name, district, location_x, location_y, build_date, avail_beds) values(?,?,?,?,?,?,?)";
     private static String COUNT_ALL_HOSPITAL_QUERY = "SELECT COUNT(*) FROM hospital";
+    private static String GET_ALL_HOSPITALS_QUERY = "SELECT * FROM hospital";
+    private static String GET_AVAILABLE_BEDS = "SELECT avail_beds from hospital where id = ?";
+    private static String UPDATE_AVAIL_BEDS_QUERY = "UPDATE hospital SET avail_beds = ? WHERE id = ?";
 
     /**
      * load hospital data fro database according to hospital id
-     * @param hospital
+     * @param id
      * @return
      */
     public static Hospital loadHospitalFromDB(String id) {
@@ -54,8 +58,9 @@ public class HospitalRepository {
 
     /**
      * Insert hospital to a database
-     * @param user
-     */
+     * @param hospital
+     * @return
+     */ 
     public static boolean insertIntoHospital(Hospital hospital) {
 
         Connection connection = null;
@@ -127,4 +132,118 @@ public class HospitalRepository {
 
         return count; 
     } 
+
+
+    /**
+     * Get the Hospitals list from the Database
+     * @return
+     */
+    public static ArrayList<Hospital> getHospitalList(){
+        
+        ResultSet resultSet = null;
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        ArrayList<Hospital> hospitalList = new ArrayList<Hospital>();
+
+        try {
+            connection = DBConnectionPool.getInstance().getConnection();
+            statement = connection.prepareStatement(GET_ALL_HOSPITALS_QUERY);
+            resultSet = statement.executeQuery();
+
+            Hospital hospital;
+            while (resultSet.next()) {
+                hospital = new Hospital(resultSet.getString("id"));
+                hospital.setName(resultSet.getString("name"));
+                hospital.setDistrict(resultSet.getString("district"));
+                hospital.setLocationX(resultSet.getInt("location_x"));
+                hospital.setLocationY(resultSet.getInt("location_y"));
+                hospital.setBuildDate(resultSet.getDate("build_date"));
+                hospital.setAvailBeds(resultSet.getInt("avail_beds"));
+                hospitalList.add(hospital);
+            }
+
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+        finally
+        {
+            DBConnectionPool.getInstance().close(statement);
+            DBConnectionPool.getInstance().close(connection);
+        }
+
+        return hospitalList;
+    } 
+
+    /**
+     * Return the available bed no for a hospital
+     * @return
+     */
+    public static int getAvailabelBedNo(Hospital hospital){
+        
+        ResultSet resultSet = null;
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        int bedNo = 0;
+        
+        try {
+            connection = DBConnectionPool.getInstance().getConnection();
+            statement = connection.prepareStatement(GET_AVAILABLE_BEDS);
+            statement.setString(1, hospital.getId());
+            resultSet = statement.executeQuery();
+            resultSet.next();
+            bedNo = 11 - resultSet.getInt(1);   // result is available beds count
+
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+        finally
+        {
+            DBConnectionPool.getInstance().close(statement);
+            DBConnectionPool.getInstance().close(connection);
+        }
+
+        return bedNo; 
+    } 
+
+
+    /**
+     * Update hospital available beds in the database
+     * @param hospital
+     * @return
+     */ 
+    public static boolean updateAvailBeds(Hospital hospital) {
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        int result = 0;
+
+        try {
+
+            connection = DBConnectionPool.getInstance().getConnection();
+
+            statement = connection.prepareStatement(UPDATE_AVAIL_BEDS_QUERY);
+            statement.setInt(1, hospital.getAvailBeds());
+            statement.setString(2, hospital.getId());
+
+            System.out.println(statement);
+            result = statement.executeUpdate();
+            
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+        finally
+        {
+            DBConnectionPool.getInstance().close(statement);
+            DBConnectionPool.getInstance().close(connection);
+        }
+
+        // if insert into is successful return true
+        if (result > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- Back-End/ncms/src/main/java/com/sparkx/damsy/service/HospitalService.java
+++ Back-End/ncms/src/main/java/com/sparkx/damsy/service/HospitalService.java
@@ -1,6 +1,25 @@
 package com.sparkx.damsy.service;
 
+import com.sparkx.damsy.models.Hospital;
+import com.sparkx.damsy.models.Patient;
+
 public class HospitalService {
 
+    /**
+     * Method to get distant to a patient from a hospital
+     * 
+     * @param hospital
+     * @param patient
+     * @return
+     */
+    public static double getDistanceToPatient(Hospital hospital, Patient patient) {
+
+        int xDiff = Math.abs(hospital.getLocationX() - patient.getLocationX());
+        int yDiff = Math.abs(hospital.getLocationX() - patient.getLocationX());
+
+        double distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+
+        return distance;
+    }
     
 }
