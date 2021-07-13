c7e3097e37dc07f6545f6794ba88bec4777ed003
DamsyDeSilva <damsydesilva@gmail.com>
2021-07-13 15:51:51 +0530

    fixes

--- Back-End/ncms/src/main/java/com/sparkx/damsy/controllers/HospitalController.java
+++ Back-End/ncms/src/main/java/com/sparkx/damsy/controllers/HospitalController.java
@@ -71,14 +71,4 @@ public class HospitalController extends HttpServlet {
         }
     }
 
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // super.doPut(req, resp);
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // super.doDelete(req, resp);
-    }
-
 }
